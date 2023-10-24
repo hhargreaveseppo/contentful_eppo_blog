187e378e327789b86f42572f3b601f0d9f66efad
Heather <heather@geteppo.com>
2023-10-24 10:02:32 -0700

    blog post repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# contentful_eppo_blog
---
+++ index.js
@@ -0,0 +1,34 @@
+import { init } from "@eppo/node-server-sdk";
+import * as EppoSdk from "@eppo/node-server-sdk";
+
+import contentful from "contentful";
+import 'dotenv/config'
+
+
+// Eppo Specific methods
+await init({
+    apiKey: process.env.EPPO_SDK_KEY
+  });
+
+const userid = "id" + Math.random().toString(16).slice(2)
+
+const eppoClient = EppoSdk.getInstance();
+
+const homepage_entry_id = eppoClient.getStringAssignment(
+    userid, // unique identifier for the user
+    "homepage_cta", // flag key from Eppo UI
+    )
+console.log('homepage_entry_id', homepage_entry_id)
+
+//contentful specific methods
+
+const contentfulClient = await contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+})
+
+const entry = await contentfulClient.getEntry(homepage_entry_id)
+console.log(entry) // see the different assignments that a user received
+
+
+
